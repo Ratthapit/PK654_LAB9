b266e45bd9339760661be92b09825b88014dbefe
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2021-12-07 14:54:38 +0700

    lab9_3.cpp Completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,40 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int a,h,p;
+	cout <<"Enter your age: ";
+	cin >> a;
+	
+	string ss;
+	if(a<=20){
+	    cout <<"Enter your height: ";
+	    cin >> h;
+	
+	    if(h>=175){
+	        cout <<"Enter your property: ";
+	        cin >> p;
+	    
+	        ss= p>69000000 ? "MARRIED" : "ONE-NIGHT-STAND";
+	    }
+	    else{
+	        ss= h>=160 ? "FRIEND" : "UNFRIEND";
+	    }
+	}
+	else{
+	    cout <<"Enter your property: ";
+	    cin >> p;
+	
+	    ss= a>=30 ? "UNFRIEND" : p>10000000 ? "BEST FRIEND" : "UNFRIEND";
+	}
+	
+	cout <<"Your status = "<<ss;
+
+	return 0;
+}
+
+
 /*
 "Enter your age: "
 "Enter your height: "
